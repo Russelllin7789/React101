20525fd1a6f79f88f061c4cd62bc3d3cece62185
RussellLin <superbr7789@gmail.com>
2021-10-31 22:26:41 +0800

    init React/webpack/babel related settings

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: './jsx/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
