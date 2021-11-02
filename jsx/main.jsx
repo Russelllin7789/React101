5a0830a7959fa2395f3432a109fda3270d922682
RussellLin <superbr7789@gmail.com>
2021-11-02 08:13:54 +0800

    init state & setState

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -1,4 +1,20 @@
-ReactDOM.render(
-  <h1>Hello Russell using React!</h1>,
-  document.getElementById('root')
-)
+class Test extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '123'
+    }
+  }
+  inputHandler(e) {
+    this.setState({ value: e.target.value })
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={this.inputHandler.bind(this)} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'))
