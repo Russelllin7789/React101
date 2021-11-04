13bdc3d41c26aac8b98090378a7442171e2e73a0
RussellLin <superbr7789@gmail.com>
2021-11-04 21:10:02 +0800

    add districts list and interactive postal codes

--- jsx/AddressPicker.jsx
+++ jsx/AddressPicker.jsx
@@ -4,18 +4,45 @@ class AddressPicker extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      city: '新竹市'
+      city: '宜蘭縣',
+      district: '',
+      postalCode: ''
     }
 
     this.postalCode = TaiwanPostalCode
     this.cities = Object.keys(this.postalCode)
   }
 
-  selectedCity = (e) => {
-    let { name, value } = e.target
-    this.setState({ [name]: value }, () => console.log(this.state))
+  handler = (name, value) => {
+    let mergeObject = {}
+
+    if (name === 'city' && this.state.city !== value) {
+      //利用 object 複寫的特性，在重新選擇城市後，清空區域 && 郵遞區號
+      mergeObject['district'] = ''
+      mergeObject['postalCode'] = ''
+    } else if (name === 'district' && this.state.district !== value) {
+      const cityData = this.postalCode[this.state.city]
+      const postalCode = cityData[value]
+      mergeObject['postalCode'] = postalCode
+    }
+
+    return mergeObject
   }
 
+  selectedAction = (e) => {
+    let { name, value } = e.target
+
+    const mergeObject = this.handler(name, value)
+    this.setState({ ...mergeObject, [name]: value }, () => console.log(this.state))
+  }
+
+  getDistricts = (districts) => {
+    return districts.map((district) => {
+      return (<option key={district} value={district}>{district}</option>)
+    })
+  }
+
+  //這段也可以放在 render 裡面
   getCityOptions = (cities) => {
     return cities.map((city) => {
       return (<option key={city} value={city}>{city}</option>)
@@ -24,20 +51,25 @@ class AddressPicker extends React.Component {
 
   render() {
     const cityOptions = this.getCityOptions(this.cities)
+    const cityData = Object.keys(this.postalCode[this.state.city])
+    const districts = this.getDistricts(cityData)
     return (
       <div className="container">
-        <h1>Address Picker
-          <div>
-            <label>城市</label>
-            <select name="city" onChange={this.selectedCity} value={this.state.city}>
-              {cityOptions}
-            </select>
-            <label>區域</label>
-            <select name="area" >
-              <option></option>
-            </select>
-          </div>
-        </h1>
+        <h1>Address Picker</h1>
+        <div>
+          <label>城市</label>
+          <select name="city" onChange={this.selectedAction} value={this.state.city}>
+            {cityOptions}
+          </select>
+          <br />
+          <label>區域</label>
+          <select name="district" onChange={this.selectedAction} value={this.state.district} >
+            {districts}
+          </select>
+          <br />
+          <label>郵遞區號</label>
+          <input name="postalCode" onChange={this.selectedAction} value={this.state.postalCode} disabled={true} />
+        </div>
       </div>
     )
   }
