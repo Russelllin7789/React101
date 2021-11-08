a142e07d5ade3bbf0ace45ba2298ca50a6736ed9
RussellLin <superbr7789@gmail.com>
2021-11-08 08:31:29 +0800

    Add new jsx for other component rendering

---
+++ jsx/ReceiptType.jsx
@@ -0,0 +1,18 @@
+class ReceiptType extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render = () => {
+    return (
+      <div>
+        <h1>Hello!!!Russell~~~</h1>
+      </div>
+    )
+  }
+}
+
+export default ReceiptType;
