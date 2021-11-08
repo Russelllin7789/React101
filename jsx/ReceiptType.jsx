26ecf177b63c8d77322c4ba6920894214c0f13be
RussellLin <superbr7789@gmail.com>
2021-11-08 22:23:26 +0800

    Correct the logic for receipt radio button

--- jsx/ReceiptType.jsx
+++ jsx/ReceiptType.jsx
@@ -2,7 +2,7 @@ class ReceiptType extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      receiptType: 2,
+      receiptType: '2',
       taxId: '',
       receiptOptions: ['byMail']
     }
@@ -24,12 +24,12 @@ class ReceiptType extends React.Component {
         <div>
           <span>發票類型</span><br />
           <label>
-            <input type="radio" name="receiptType" value="2" checked={this.state.receiptType === 2} onChange={this.handler} />
+            <input type="radio" name="receiptType" value="2" checked={this.state.receiptType === '2'} onChange={this.handler} />
             個人
           </label>
           <br />
           <label>
-            <input type="radio" name="receiptType" value="3" checked={this.state.receiptType === 3} onChange={this.handler} />
+            <input type="radio" name="receiptType" value="3" checked={this.state.receiptType === '3'} onChange={this.handler} />
             公司
             統一編號
             <input type="text" name="taxId" value={this.state.taxId} onChange={this.handler} />
